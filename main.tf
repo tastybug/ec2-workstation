d3759bb0d026f096b889d3b7cf01ca386423742e
Philipp Bartsch <tastybug@tastybug.com>
2021-08-03 09:59:28 +0200

    Allow outgoing traffic

--- main.tf
+++ main.tf
@@ -47,6 +47,12 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0 
+    to_port     = 0
+    protocol    = -1 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "ec2_instance" {
